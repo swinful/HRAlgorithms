b279b4e4999ff10c701c032823692e98eba5955c
Samuel A. Winful <samuel@winful.com>
2016-02-17 07:58:59 -0500

    Updated second implementation of Library Fine.

--- Implementation/Implementation_LibraryFine/Implementation_LibraryFine/main.swift
+++ Implementation/Implementation_LibraryFine/Implementation_LibraryFine/main.swift
@@ -1,36 +1,67 @@
+////
+// main.swift
+// Implementation_LibraryFine
 //
-//  main.swift
-//  Implementation_LibraryFine
-//
-//  Created by Samuel A WINFUL on 2/14/16.
-//  Copyright © 2016 Samuel A WINFUL. All rights reserved.
-//
+// Created by Samuel A WINFUL on 2/14/16.
+// Copyright © 2016 Samuel A WINFUL. All rights reserved.
+////
+import Foundation
 
-var fine = 0
-
-struct Date {
+class Date {
   var day   = 1
   var month = 1
   var year  = 2000
   
   func readDate() {
-    let dateLine = readLine()!.charactersSeperatedBy(" ").
+    let dateLine = readLine()!.componentsSeparatedByString(" ").map { Int(String($0))! }
+    setDate(dateLine[0], m: dateLine[1], y: dateLine[2])
+  }
+  
+  func monthsLate(otherDate: Date) -> Int {
+    // Calculate late months
+    return self.month - otherDate.month
+  }
+  
+  func daysLate(otherDate: Date) -> Int {
+    // Calculate late days
+    return abs(self.day - otherDate.day)
+  }
+  
+  func onOrBefore(otherDate: Date) -> Bool {
+    return year <= otherDate.year
   }
   
   private func setDate(d: Int, m: Int, y: Int) {
-    day   = d
-    month = m
-    year  = y
+    (day, month, year) = (d, m, y)
+  }
+} // class Date ends
+
+// MARK: Main ============================================================
+
+var fine = 0
+let bookReturnedOn = Date()
+bookReturnedOn.readDate()
+
+let bookExpectedOn = Date()
+bookExpectedOn.readDate()
+
+// If the book was returned after the expected year, issue the max fine.
+if bookReturnedOn.year > bookExpectedOn.year {
+  fine = 10_000
+
+// otherwise, if the book was returned in the same year
+} else if bookReturnedOn.year == bookExpectedOn.year {
+  
+  // and same month as they expected return date
+  if bookReturnedOn.month == bookExpectedOn.month && bookReturnedOn.day > bookExpectedOn.day {
+    
+    // calculate the number of days late for the fine.
+    let daysLate = bookReturnedOn.daysLate(bookExpectedOn)
+    fine = 15 * daysLate
+  // Otherwise, if the book was returned on a later month
+  } else if bookReturnedOn.month > bookExpectedOn.month {
+    fine = 500 * bookReturnedOn.monthsLate(bookExpectedOn)
   }
 }
 
-if bookReturned <= returnDate {
-  // No fine charged. Fine is zero (0)
-} else if bookReturned.month == returnDate.month {
-  // fine = 15 Hackos x number of days late.
-} else if bookReturned.month != returnDate.month {
-  if bookReturned.year == returnedDate.year {
-    // fine = 500 Hackos x number of late months.
-  } else if bookReturned.year != returnDate.year {
-    // fine = 10,000 Hackos
-}
+print(fine)
