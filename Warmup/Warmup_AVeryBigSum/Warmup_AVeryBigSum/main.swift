bc33ea2debbc0da287760de391dc264e9e9888ab
Samuel A. Winful <samuel@winful.com>
2016-03-05 19:18:59 -0500

    Saving changes.

--- Warmup/Warmup_AVeryBigSum/Warmup_AVeryBigSum/main.swift
+++ Warmup/Warmup_AVeryBigSum/Warmup_AVeryBigSum/main.swift
@@ -10,6 +10,5 @@ import Foundation
 let n = Int(readLine()!)!
 let nNumbers = readLine()!.characters.split(" ").map {Int(String($0))!}
 
-var sum = 0
-nNumbers.forEach { sum += $0 }
+let sum = nNumbers.reduce(0) { $0 + $1 }
 print(sum)
